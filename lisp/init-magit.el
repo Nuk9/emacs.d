583d47583dd3f24cf763d5d50c48d34cf031065f
xzhao <i@xuzhao.net>
2016-01-13 22:29:40 -0500

    Magit now builtin.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -3,7 +3,6 @@
 ;;; Author: Xu Zhao (i@xuzhao.net)
 
 ;;; Code:
-(require-package-load 'magit)
 
 (global-set-key (kbd "M-<f12>") 'magit-status)
 (setq-default magit-completing-read-function 'ivy-completing-read)
