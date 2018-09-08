75a9a0ee0e1b018a5129e1947464abccb4b2e6f4
xuzhao9 <i@xuzhao.net>
2018-09-07 21:27:10 -0400

    Changed Emacs theme.

--- init.el
+++ init.el
@@ -51,23 +51,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(delete-selection-mode nil)
- '(package-selected-packages
-   (quote
-    (edit-server racket-mode yaml-mode yaml-tomato haskell-mode yasnippet telephone-line swiper rainbow-delimiters racer powerline nhexl-mode moe-theme markdown-mode+ magit key-chord html-to-markdown flycheck-rust find-file-in-project exec-path-from-shell evil-leader company-racer company-lsp color-theme-sanityinc-solarized cmake-mode auto-sudoedit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-preview-common ((t (:inherit default :foreground "darkred"))))
- '(company-tooltip ((t (:inherit nil :background "grey" :foreground "black"))))
- '(company-tooltip-annotation ((t (:inherit company-tooltip :foreground "black"))))
- '(company-tooltip-common ((t (:inherit company-tooltip :foreground "black"))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :foreground "white"))))
- '(company-tooltip-selection ((t (:background "steelblue" :foreground "white")))))
--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -5,6 +5,23 @@
 ;;; Code:
 
 (setq-default c-default-style "linux")
+(setq-default c-basic-offset 4)
+
+; style I want to use in c++ mode
+(c-add-style "cpp-style" 
+	     '("stroustrup"
+	       (indent-tabs-mode . nil)        ; use spaces rather than tabs
+	       (c-basic-offset . 4)            ; indent by four spaces
+	       (c-offsets-alist . ((inline-open . 0)  ; custom indentation rules
+				   (brace-list-open . 0)
+				   (statement-case-open . +)))))
+
+(defun my-c++-mode-hook ()
+  (c-set-style "cpp-style")
+  (auto-fill-mode)         
+  (c-toggle-auto-hungry-state 1))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
 (provide 'init-cc)
 
