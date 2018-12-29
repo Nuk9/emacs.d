ce74380eb073b71253f3f4c02a6e86b97cd8ab56
xuzhao9 <i@xuzhao.net>
2018-12-29 17:04:34 -0500

    Fixed bug in shell-toggle.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -2,9 +2,11 @@
 ;;; Shell toggle
 ;;; Commentary:
 ;;; Author: Xu Zhao (i@xuzhao.net)
+;;; Code:
 
 (require 'shell-toggle)
 
 (global-set-key (kbd "<f4>") 'shell-toggle)
 
 (provide 'init-shell)
+;;; init-shell.el ends here
